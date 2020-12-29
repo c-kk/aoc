c1a80889c912ed2b566cbcdbd52d698425834c63
c-kk <>
2020-12-29 20:04:53 +0100

    Added 2019 day 2

--- 2019/day01/solve.py
+++ 2019/day01/solve.py
@@ -1,10 +1,24 @@
-lines = open("2019/day1/data.txt").read().split('\n')
+lines = open("data.txt").read().split('\n')
 
+# Part 1
 total = 0
 for mass in lines:
     fuel = int(mass) // 3 - 2  # divide by 3, round down, subtract 2
     total += fuel
 print(total)
 
-pass
+# Part 2
+total = 0
+for mass in lines:
+    extra_mass = mass
+    fuel = 0
 
+    while True:
+        extra_fuel_needed = int(extra_mass) // 3 - 2  # divide by 3, round down, subtract 2
+        if extra_fuel_needed <= 0:
+            break
+        fuel += extra_fuel_needed
+        extra_mass = extra_fuel_needed
+
+    total += fuel
+print(total)
