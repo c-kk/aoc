4a335762e8f7360578bb5b4593107bb1ed779e6c
c-kk <>
2021-01-03 15:51:54 +0100

    Shortened 2019 day 15 and added luca, tprint and timing to lib

--- 2019/day15/bfs.py
+++ 2019/day15/bfs.py
@@ -1,8 +1,4 @@
-import numpy as np
-
 # https://stackoverflow.com/questions/47896461/get-shortest-path-to-a-cell-in-a-2d-array-in-python
-
-# Known grid dimensions
 def breadth_first_search():
     pos    = (9, 1)
     wall   = "#"
@@ -32,74 +28,5 @@ def breadth_first_search():
                 print('paths', paths)
                 print('seen', seen)
 
-# path = breadth_first_search()
-# print('Shortest path', path)
-
-secret_pos = (8,2)
-
-# Unknown grid dimensions
-def unknown_grid(move_cmd):
-    grid = ["..........",
-            "..*#...##.",
-            "..##...#..",
-            ".....###..",
-            ".........."]
-
-    moves  = {1: (0, 1), 2: (0, -1), 3: (-1, 0), 4: (1, 0)}
-    move   = moves[move_cmd]
-    nw_pos = tuple(np.add(pos, move))
-    
-    try:
-        x,y  = nw_pos
-        char = grid[y][x]
-    except:
-        char = '#'
-
-    if char != '#':
-        secret_pos = nw_pos
-
-    return char
-
-def breadth_first_search_unknown():
-    path  = [((0, 0), 0)]
-    paths = [path]
-    seen = set(path)
-    while paths:
-        path = paths.pop(0)
-        pos, move_cmd = path[-1]
-
-        print(pos, move_cmd)
-        exit()
-        # if path != (0, 0):
-        #     diff = 
-        
-        moves = {1: (0, 1), 2: (0, -1), 3: (-1, 0), 4: (1, 0)}
-        for move_cmd, move in moves.items():
-            # Calculate new position
-            nw_pos = tuple(np.add(pos, move))
-            
-            # Continue if already seen position
-            if nw_pos in seen:
-                continue
-
-            # Send move to droid and receive character
-            char = unknown_grid(move_cmd)
-
-            # Continue when a wall is hit
-            if char == '#':
-                continue
-
-            # Finish when goal is reached
-            if char == '*':
-                return path + [nw_pos]
-
-            # Reverse step when a path is reached
-            if char == '.':
-                _ = unknown_grid((move_cmd - 2) % 4)
-                paths.append(path + [nw_pos], move_cmd)
-                seen.add(nw_pos)            
-
-            # print(char)
-
-path = breadth_first_search_unknown()
+path = breadth_first_search()
 print('Shortest path', path)
