04b70447221f7a215f692d981fc13ecab504d8ef
c-kk <>
2022-12-04 13:51:27 +0100

    Add partial solution for 2021 day 12 in Go

--- 2021-go/day12/solve.go
+++ 2021-go/day12/solve.go
@@ -13,19 +13,10 @@ func main() {
 	fmt.Println(Answer2(puzzleInput))
 }
 
-type cave struct {
-	key     string
+type Cave struct {
+	key string
 	size    string
-	visited bool
-	targets []*cave
-}
-
-func (t cave) String() string {
-	targetKeys := []string{}
-	for _, target := range t.targets {
-		targetKeys = append(targetKeys, target.key)
-	}
-	return fmt.Sprintf("{%v %v %v %v}", t.key, t.size, t.visited, targetKeys)
+	targets []Cave
 }
 
 type Path struct {
@@ -35,158 +26,100 @@ type Path struct {
 func Answer1(puzzleInput string) int {
 	lines := strings.Split(puzzleInput, "\n")
 	caves := convertLinesToCaves(lines)
-	printCaves(caves)
+	// printCaves(caves)
+	finishedPaths := [][]string{}
+	activePaths := append([][]string{}, []string{"start"})
 
-	paths := [][]string{}
-	path := []string{"start"}
-	paths = append(paths, path)
-	printPaths(paths)
-
-	oldPathCount := 1
+	doubleVisited := true
 	step := 0
+	subStep := 0
 
-	for step < 3 {
-		fmt.Println("###### STEP", step, "######")
+	for {
+		newActivePaths := [][]string{}
 
-		newPaths := [][]string{}
-
-		for _, path := range paths {
-			fmt.Println("Path:", path)
-
-			caveKey := path[len(path)-1]
-			cave := caves[caveKey]
-			targets := filterTargets(cave.targets, path)
-			fmt.Println("Targets:", targets)
-
-			if len(targets) == 0 {
-				fmt.Println("No allowed target found")
-				fmt.Println()
-				newPaths = append(newPaths, path)
-			} else {
-				for index, target := range targets {
-					fmt.Println("Index", index, "Target", target)
-					newPath := append(path, target.key)
-
-					newPaths = append(newPaths, newPath)
-				}
+		for _, activePath := range activePaths {
+			subStep += 1
+			caveKey := activePath[len(activePath)-1]
+			if caveKey == "end" {
+				finishedPaths = append(finishedPaths, activePath)
 			}
 
-			printPaths(newPaths)
+			cave := caves[caveKey]
+			targets, _ := filterTargets(cave.targets, activePath, doubleVisited)
+
+			if len(targets) > 0 {
+				for _, target := range targets {
+					newActivePath := append([]string{}, activePath...)
+					newActivePath = append(newActivePath, target.key)
+					newActivePaths = append(newActivePaths, newActivePath)
+				}
+			}
 		}
 
-		// 	currentCave := caves["start"]
-		// 	path := []string{}
-		// 	path = append(path, currentCave.key)
+		fmt.Println("Step", step, "Substeps", subStep, "Finished paths", len(finishedPaths), "Active paths", len(newActivePaths))
 
-		// 	for {
-		// 		currentCave.visited = true
-		// 		// fmt.Println("Current cave:", currentCave.key, currentCave)
-
-		// 		paths := append(validPaths, invalidPaths...)
-		// 		targets := filterTargets(currentCave.targets, path, paths)
-		// 		// fmt.Println("Targets:", targets)
-
-		// 		if len(targets) == 0 {
-		// 			// fmt.Println("No allowed target found")
-		// 			// fmt.Println("Path:", path)
-		// 			// fmt.Println()
-		// 			if !doesPathExist(path, invalidPaths) {
-		// 				invalidPaths = append(invalidPaths, path)
-		// 			}
-		// 			path = []string{}
-		// 			break
-		// 		}
-
-		// 		target := targets[0]
-		// 		path = append(path, target.key)
-
-		// 		if target.key == "end" {
-		// 			// fmt.Println("End of path reached")
-		// 			// fmt.Println("Path:", path)
-		// 			// fmt.Println()
-		// 			validPaths = append(validPaths, path)
-		// 			path = []string{}
-		// 			break
-		// 		}
-
-		// 		// fmt.Println("Selected target:", target)
-		// 		// fmt.Println("Path:", path)
-		// 		// fmt.Println()
-		// 		currentCave = target
-		// 	}
-
-		// 	// fmt.Println("Valid paths:", validPaths)
-		// 	// fmt.Println("Invalid paths:", invalidPaths)
-		// 	validPathCount := len(validPaths)
-		// 	invalidPathCount := len(invalidPaths)
-
-		// 	fmt.Println("Step", step, "Valid paths", validPathCount)
-		// 	// fmt.Println()
-
-		newPathCount := len(newPaths)
-		if newPathCount == oldPathCount {
+		if len(newActivePaths) == 0 {
 			break
 		}
-		paths = newPaths
-		oldPathCount = newPathCount
+
+		activePaths = newActivePaths
 		step += 1
 	}
 
-	return 10
+	return len(finishedPaths)
 }
 
-func printPaths(paths [][]string) {
-	fmt.Println("Paths:")
-	for _, path := range paths {
-		fmt.Println(path)
-	}
-}
+func Answer2(puzzleInput string) int {
+	lines := strings.Split(puzzleInput, "\n")
+	caves := convertLinesToCaves(lines)
+	// printCaves(caves)
+	finishedPaths := [][]string{}
+	activePaths := append([][]string{}, []string{"start"})
 
-// Return only the targets that are allowed to go to
-func filterTargets(targets []*cave, path []string) []*cave {
-	filtered := []*cave{}
-	for _, target := range targets {
-		// It's not allowed if target is small and visited
-		if target.size == "small" {
-			visited := false
-			for _, caveKey := range path {
-				if target.key == caveKey {
-					visited = true
+	step := 0
+	subStep := 0
+
+	for {
+		newActivePaths := [][]string{}
+
+		for _, activePath := range activePaths {
+			doubleVisited := false
+			subStep += 1
+			caveKey := activePath[len(activePath)-1]
+			if caveKey == "end" {
+				finishedPaths = append(finishedPaths, activePath)
+			}
+
+			cave := caves[caveKey]
+
+			targets := []Cave{}
+			targets, doubleVisited = filterTargets(cave.targets, activePath, doubleVisited)
+
+			if len(targets) > 0 {
+				for _, target := range targets {
+					newActivePath := append([]string{}, activePath...)
+					newActivePath = append(newActivePath, target.key)
+					newActivePaths = append(newActivePaths, newActivePath)
 				}
 			}
-			if visited {
-				continue
-			}
-
 		}
 
-		filtered = append(filtered, target)
-	}
-	return filtered
-}
+		fmt.Println("Step", step, "Substeps", subStep, "Finished paths", len(finishedPaths), "Active paths", len(newActivePaths))
 
-// Check if the path exists to prevent duplicate paths
-func doesPathExist(path []string, paths [][]string) bool {
-	exists := false
-	for _, existingPath := range paths {
-		pathString := strings.Join(path, ",")
-		existingPathString := strings.Join(existingPath, ",")
-		if pathString == existingPathString {
-			exists = true
+		if len(newActivePaths) == 0 {
+			break
 		}
+
+		activePaths = newActivePaths
+		step += 1
 	}
-	return exists
+
+	return len(finishedPaths)
 }
 
-func printCaves(caves map[string]*cave) {
-	fmt.Println("Caves:")
-	for _, cave := range caves {
-		fmt.Println(cave)
-	}
-}
-
-func convertLinesToCaves(lines []string) map[string]*cave {
-	caves := map[string]*cave{}
+// Convert input to caves
+func convertLinesToCaves(lines []string) map[string]Cave {
+	caves := map[string]Cave{}
 
 	// Create caves
 	for _, line := range lines {
@@ -200,7 +133,7 @@ func convertLinesToCaves(lines []string) map[string]*cave {
 				size = "nil"
 			}
 
-			caves[caveKey] = &cave{caveKey, size, false, []*cave{}}
+			caves[caveKey] = Cave{caveKey, size, []Cave{}}
 		}
 	}
 
@@ -215,19 +148,56 @@ func convertLinesToCaves(lines []string) map[string]*cave {
 		// Add the 'to' location to the 'from' cave destinations
 		// Restrictions: you can't go the start or exit from the end
 		if to != "start" && from != "end" {
-			caveFrom.targets = append(caveFrom.targets, caves[to])
+			caveFrom.targets = append(caveFrom.targets, caveTo)
+			caves[from] = caveFrom
 		}
 
 		// Reverse the route: also add the 'from' location to the 'to' cave destinations
 		// Restrictions: you can't reverse from the end and you can't go back to the start
 		if to != "end" && from != "start" {
 			caveTo.targets = append(caveTo.targets, caveFrom)
+			caves[to] = caveTo
 		}
 	}
 	return caves
 }
 
-func Answer2(puzzleInput string) int {
-	// lines := strings.Split(puzzleInput, "\n")
-	return 0
+// Return only the targets that are allowed to go to
+func filterTargets(targets []Cave, path []string, doubleVisited bool) ([]Cave, bool) {
+	filtered := []Cave{}
+	for _, target := range targets {
+		// It's not allowed if target is small and visited
+		if target.size == "small" {
+			visited := false
+			for _, caveKey := range path {
+				if target.key == caveKey {
+					if doubleVisited == false {
+						doubleVisited = true
+					} else {
+						visited = true
+					}
+				}
+			}
+			if visited {
+				continue
+			}
+		}
+
+		filtered = append(filtered, target)
+	}
+	return filtered, doubleVisited
+}
+
+func printPaths(paths [][]string) {
+	fmt.Println("Paths:")
+	for _, path := range paths {
+		fmt.Println(path)
+	}
+}
+
+func printCaves(caves map[string]Cave) {
+	fmt.Println("Caves:")
+	for _, cave := range caves {
+		fmt.Println(cave)
+	}
 }
