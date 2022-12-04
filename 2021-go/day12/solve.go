f1d0de5c5da1583004a9570ee8806cf1d0aee24c
c-kk <>
2022-12-04 13:51:27 +0100

    Add explanation for usage of primes to 2021 day 12 solution

--- 2021-go/day12/solve.go
+++ 2021-go/day12/solve.go
@@ -86,7 +86,8 @@ func isAllowed(targetId int, path []int, maxDoubleVisits int) bool {
 	countDoubleVisits := 0
 	multiple := targetId
 	for _, caveId := range path {
-		if multiple%caveId == 0 {
+		exists := multiple%caveId == 0
+		if exists {
 			countDoubleVisits += 1
 			if countDoubleVisits > maxDoubleVisits {
 				return false
@@ -98,6 +99,8 @@ func isAllowed(targetId int, path []int, maxDoubleVisits int) bool {
 	return true
 }
 
+// Small caves have positive id's
+// The other caves have negative id's
 const START_CAVE_ID int = -100
 const END_CAVE_ID int = -101
 const BIG_CAVE_START_ID int = -200
@@ -106,6 +109,7 @@ const BIG_CAVE_START_ID int = -200
 func convertLinesToCaves(lines []string) map[int][]int {
 	bigCaveId := BIG_CAVE_START_ID
 
+	// Using primes as id's for the small caves. This makes finding duplicates easy. See multiple%caveId == 0 in isAllowed function
 	smallCaveId := 0
 	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541}
 
